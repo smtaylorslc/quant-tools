f89fab01129778e337bde8550a015366753f6dd6
Spencer <smtaylorslc@gmail.com>
2017-05-14 06:16:16 -0600

    Initial Working Efficient Frontier
    Efficient frontier built using Bokeh, accessing data using quandl's WIKI data source.

--- electron_app/src/js/create_js.js
+++ electron_app/src/js/create_js.js
@@ -1,20 +1,22 @@
 var plt = Bokeh.Plotting;
 var request = require('request')
 import $ from 'jquery'
-var options = 'https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json'
+var options = [
+  'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=20150101&date.lt=20160101&ticker=MSFT,AAPL&api_key=ENTER_API_KEY'
+]
 var M = 100;
 var moment = require('moment')
+var zerorpc = require('zerorpc')
+
 // create a data source
 function createChart(xx,yy) {
   var source = new Bokeh.ColumnDataSource({
       data: { x: xx, y: yy }
   });
-  console.log(source)
 
   // make the plot and add some tools
   var tools = "pan,crosshair,wheel_zoom,box_zoom,reset,save";
-  var p = plt.figure({ title: "Colorful Scatter", tools: tools });
-  console.log(p)
+  var p = plt.figure({ title: "Markowitz Efficient Frontier", tools: tools });
   // call the circle glyph method to add some circle glyphs
   var circles = p.circle({ field: "x" }, { field: "y" }, {
       source: source,
@@ -26,22 +28,23 @@ function createChart(xx,yy) {
 
   plt.show(p, "#plot");
 }
-function callback(info) {
-  //var info = JSON.parse(body);
-  //console.log(info)
-  var xx = info.dataset.data.map((obj) => {
-    return moment(obj[0]).toDate()
-  })
-  console.log(xx)
-  var yy = info.dataset.data.map((obj) => {
-    return obj[1]
-  })
-  console.log(yy)
-  createChart(xx,yy)
-}
-var req  = $.get(options)
 
-req.done(function(response) {
-  callback(response)
-})
+function callback(info) {
+  var client = new zerorpc.Client();
+  client.connect("tcp://127.0.0.1:4242");
+  var xx = info.datatable
+  client.invoke("getReturns",xx, function(error, res, more) {
+    xx = res
+    console.log(xx)
+    createChart(xx['stds'],xx['means'])
+  })
+}
+for (var i = 0; i < options.length; i++) {
+  var req = $.get(options[i])
+  req.done(function(response) {
+    console.log(response)
+    callback(response)
+  })
+}
+
 // set up some data
--- start-server.py
+++ start-server.py
@@ -1,9 +1,59 @@
 import zerorpc
+import pandas as pd
+import logging
+import numpy as np
+logging.basicConfig()
 
-class HelloRPC(object):
-    def hello(self, name):
-        return "Hello, %s" % name
+def rand_weights(n):
+    k = np.random.rand(n)
+    return k / sum(k)
 
-s = zerorpc.Server(HelloRPC())
+def calcMuSigma(returns):
+    p = np.asmatrix(np.mean(returns, axis=1))
+    w = np.asmatrix(rand_weights(len(returns)))
+    C = np.asmatrix(np.cov(returns))
+
+    mu = w * p.T
+    sigma = np.sqrt(w * C * w.T)
+
+    # This recursion reduces outliers to keep plots pretty
+    if sigma > 2:
+        return calcMuSigma(returns)
+    return mu, sigma
+
+
+class ServerRPC(object):
+    def getReturns(self, prices):
+        columns = []
+        for x in prices['columns']:
+            columns.append(x['name'])
+        dataframes = {}
+        prices = pd.DataFrame(prices['data'],columns=columns)
+        # Calculate Returns
+        for item in prices['ticker'].unique():
+            dataframes[item] = prices.loc[prices['ticker']==item]
+            dataframes[item]['returns'] = dataframes[item]['close'].pct_change(1)
+            dataframes[item] = dataframes[item].dropna(subset=['returns'])
+        return_vec = [
+            list(dataframes[x]['returns']) for x in dataframes.keys()
+        ]
+        n_portfolios = 500
+        means, stds = np.column_stack([
+            calcMuSigma(return_vec)
+            for _ in xrange(n_portfolios)
+        ])
+        prices = {}
+        means = [
+            i[0] for i in means
+        ]
+        stds = [
+            i[0] for i in stds
+        ]
+        prices['means'] = means
+        prices['stds'] = stds
+        print(prices['stds'])
+        return prices
+
+s = zerorpc.Server(ServerRPC())
 s.bind("tcp://0.0.0.0:4242")
 s.run()
